fbce1bcbc597d5eda3a63c24993c5d522b6daada
Andrei Lima <dreiiziin@gmail.com>
2024-11-11 20:51:48 -0300

    chore: add plugin prettier tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="p-5 text-red-500">Hello world!</h1>
     </div>
   );
 }
